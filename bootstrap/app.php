7315fa863a20bad38c2ec23cd901fc771087aca7
hoangnm2k1 <hoangnm2k1@gmail.com>
2025-04-07 23:34:54 +0700

    adjust middleware for limiting rating

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        RateLimiter::for ('reviews', function (Request $request) {
-            return Limit::perHour(3)->by($request->user()?->id ?: $request->ip());
-        });
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
